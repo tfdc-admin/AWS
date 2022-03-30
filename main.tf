adb121bdc180529d808d87c4ff6fe09b728085e2
tfdc-admin <101254171+tfdc-admin@users.noreply.github.com>
2022-03-30 16:14:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,23 +13,8 @@ provider "aws" {
   secret_key = var.secretKey
 }
 
-data "aws_ami" "k5" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220316.0-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["849480946816"]
-}
-
 resource "aws_instance" "test" {
-  ami = data.aws_ami.k5.id
+  ami = "ami-064ff912f78e3e561"
   instance_type = "t2.micro"
   tags = {
     Name = "From_TF"
