287a0ffb2724fdf36a6f60ca4ddc812d8c873a60
tfdc-admin <101254171+tfdc-admin@users.noreply.github.com>
2022-03-30 16:10:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_ami" "k5" {
 
   filter {
     name   = "name"
-    values = [amzn2-ami-kernel-5.10-hvm-2.0.20220316.0-x86_64-gp2"]
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220316.0-x86_64-gp2"]
   }
 
   filter {
